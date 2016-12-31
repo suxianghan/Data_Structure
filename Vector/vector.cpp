e1eef8375678201f9a16663a83a8f0ba2b0c6749
LunaMystic <suxianghan@outlook.com>
2016-12-31 20:02:58 +0800

    好烦啊不想写commit
    楼上说得对

--- Vector/vector.cpp
+++ Vector/vector.cpp
@@ -1,10 +1,28 @@
 #include "vector.h"
 
 using namespace std;
+
 int main(){
-	int a,b;
-	cout<<"wow";
-	cin>>a;
-	cin>>b;
-	cout<<a<<b;
+	Vector<int> a(10,false);
+	for(int i=0;i<10;i++){
+		a.insert(i,i);
+		a.output();
+	}
+	a.remove(0);
+	a.output();
+	a.insert(0,0);
+	a.output();
+	a.remove(9);
+	a.output();
+	a.remove(4);
+	a.output();
+	a.remove(0);
+	a.output();
+	for(int i=0;i<10;i++){
+		a.insert(i,i);
+		a.output();
+	}
+	a[10] = 2;
+	cout<<a[10]<<endl;
+	a.output();
 }
--- Vector/vector.h
+++ Vector/vector.h
@@ -1,11 +1,82 @@
 #include <iostream>
 using namespace std;
 template <typename T>
-class vector{
+class Vector{
 private:
 	T* data;
 	int size;
+	int length;
+	bool isCons;
+	void expand();
 public:
-	vector();
-	~vector();
+	Vector(int sizeIn,bool alter);
+	~Vector();
+	 T& operator[](int index);
+	bool insert(int index,const T& element);
+	bool remove(int index);
+	void output();
 };
+
+template <typename T> Vector<T>::Vector(int sizeIn,bool alter):size(sizeIn),length(0),isCons(alter){
+	data = new T[sizeIn];
+}
+
+template <typename T> Vector<T>::~Vector(){
+	delete[] data;
+}
+
+template <typename T> T& Vector<T>::operator[](int index){
+	return  data[index];
+}
+template <typename T> void Vector<T>::expand(){
+	size *= 2;
+	T* tmpData = new T[size];
+	for(int i=0;i<length;i++){
+		tmpData[i] = data[i];
+	}
+	delete data;
+	data = tmpData;
+	return;
+}
+
+template <typename T> bool Vector<T>::insert(int index,const T& element){
+	if(index<0 || index>length)
+		{ return false; }
+	if(length >= size){
+		if(isCons) { return false; }
+		else { expand(); }
+	}
+	for(int i = length;i>index;i--){
+		data[i] = data[i-1];
+	}
+	data[index] = element;
+	length ++;
+	return true;
+}
+
+template <typename T> bool Vector<T>::remove(int index){
+	if(index<0 || index >= length)
+		{ return false; }
+	for(int i=index;i<length-1;i++){
+		data[i] = data[i+1];
+	}
+	length --;
+	return true;
+}
+
+template <typename T> void Vector<T>::output(){
+	if(length == 0){
+		cout<<"Nothing in yet";
+	}
+	else{
+		for(int i=0;i<length;i++){
+			cout<<data[i]<<" ";
+		}
+	}
+	cout<<endl;
+	return;
+}
+
+
+
+
